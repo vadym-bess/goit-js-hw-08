5c7769b903d8764c27851624a1c3b1310d2c1171
Vadym <besarabvadim2@gmail.com>
2022-10-23 23:00:11 +0300

    mfkfhjhg,hgj,

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-msg";
+const form = document.querySelector(".feedback-form");
+const feedbackForm = document.querySelector(".feedback-form__textarea");
+const formData = {};
+
+feedbackForm.addEventListener("input", throttle(onTextAreaInput, 1000));
+form.addEventListener("submit", onFormSubmit);
+
+form.addEventListener("input", e => {
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+});
+
+populateTextarea();
+
+
+
+function onFormSubmit(e) {
+
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+ };
+
+
+function onTextAreaInput(e) {
+
+    const message = e.target.value;
+    localStorage.setItem(STORAGE_KEY, message);
+
+};
+
+function populateTextarea(e) {
+  
+    const savedMessage = localStorage.getItem(STORAGE_KEY)
+    
+    if (savedMessage) {
+        feedbackForm.value = savedMessage;
+    };
+};
 
